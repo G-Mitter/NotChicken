e2bad1ccd1086eccd3687b218414c1368ecd977f
G-Mitter <guimotamitter@gmail.com>
2024-12-31 20:53:24 -0300

    Correção de erro ao criar projeto
    Agora foi feita há criação do projeto usando o Spring Initializer já foi colocado com as depéndencias que achei necessarias até o momento. Principalmente já pensando pra proxima etapa

---
+++ NotChicken/src/test/java/com/senacead/NotChicken/NotChicken/NotChickenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senacead.NotChicken.NotChicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotChickenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
